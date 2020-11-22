8445c4b48eb80a066fd04917bf0a3a8dcf69f475
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2020-11-22 11:06:21 -0330

    testerino 7

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -24,9 +24,8 @@ function App() {
         axios.get("http://34.122.178.86/api/markers")
         .then(res => {
             setMarkers(res.data)
-            console.log(markers);
         })
-    });
+    }, []);
 
   return (
     <div className="App">
